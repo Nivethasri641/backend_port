0d5bffe2ded37cd18ca628dcf29ec22cc1fc74db
Nivetha-26 <nivethasri0641@gmail.com>
2025-10-21 19:26:10 +0530

    nn

--- src/contactroute.js
+++ src/contactroute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { sendMessage } = require('../src/controllers/contactcontroller');
+const { sendMessage } = require('../controllers/contactcontroller');
 
 router.post('/', sendMessage);
 
